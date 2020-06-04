88da7aec8a45d588abdb6a6d205447aa4451c50f
Jason Christian H <nakamarujc@gmail.com>
2020-06-04 19:10:26 +0700

    Made slider's 'circle' antialiased, and programmed colored line

--- src/Pygame/Slider.py
+++ src/Pygame/Slider.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame.gfxdraw
 from MathUtil import clamp
 import Pygame.GlobalVariables as GVar
 from time import time
@@ -13,6 +14,7 @@ class Slider:
     sliderColor = (0, 0, 0)
     sliderColorInit = (0, 0, 0)
     sliderDraggedColor = (0, 0, 0)
+    lineColorFilled = (0, 0, 0)
 
     surfHeight = 20
 
@@ -23,7 +25,7 @@ class Slider:
 
     _dragged = False
 
-    def __init__(self, surface, coords, length, initValue=50, lineColor=(220, 220, 230), sliderColor=(46, 119, 230), sliderDraggedColor=(58, 158, 240)):
+    def __init__(self, surface, coords, length, initValue=50, lineColor=(220, 220, 230), lineColorFilled=(165, 198, 230), sliderColor=(46, 119, 230), sliderDraggedColor=(58, 158, 240)):
         self.coords = coords
         self.surfaceToDraw = surface
         self.length = length
@@ -31,18 +33,21 @@ class Slider:
         self.sliderColorInit = sliderColor
         self.sliderDraggedColor = sliderDraggedColor
         self.sliderColor = sliderColor
+        self.lineColorFilled = lineColorFilled
 
         self.value = clamp(initValue, 0, 100)
         self.sliderPos = round((initValue / 100 * length) + self.sliderRadius)
 
-        self.surface = pygame.Surface([self.length + (self.sliderRadius * 2), self.surfHeight], pygame.SRCALPHA) # Draws surface
+        self.surface = pygame.Surface([self.length + (self.sliderRadius * 2) + 2, self.surfHeight + 2], pygame.SRCALPHA) # Draws surface
 
         self.redrawSurface()
 
     def redrawSurface(self):
         self.surface.fill((255, 255, 255, 0)) # Clears surface
         pygame.draw.line(self.surface, self.lineColor, (self.sliderRadius, self.surfHeight/2), (self.length + self.sliderRadius, self.surfHeight/2), 4) # Draws slider line
-        pygame.draw.circle(self.surface, self.sliderColor, (round(self.sliderPos), self.surfHeight//2), self.sliderRadius) # Draws circle
+        pygame.draw.line(self.surface, self.lineColorFilled, (self.sliderRadius, self.surfHeight/2), (self.sliderPos, self.surfHeight/2), 4) # Draws slider line filler
+        pygame.gfxdraw.aacircle(self.surface, round(self.sliderPos), self.surfHeight//2, self.sliderRadius, self.sliderColor) # Draws circle anti-aliased outline.
+        pygame.gfxdraw.filled_circle(self.surface, round(self.sliderPos), self.surfHeight//2, self.sliderRadius, self.sliderColor) # Draws circle
 
     def getValue(self):
         return value
